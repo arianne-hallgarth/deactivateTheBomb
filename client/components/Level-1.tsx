194e5455eb7d8736d528e934dbf5a6d2a0da2f33
student <student@pop-os.localdomain>
2023-11-03 14:59:40 +1300

    add sound l2

--- client/components/Level-1.tsx
+++ client/components/Level-1.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import bombTicking from '../public/Bomb-ticking .mp3'
 
 export default function Level1Component() {
   const [isOnFire, setIsActive] = useState(true)
@@ -12,6 +13,21 @@ export default function Level1Component() {
   function handleClick() {
     setIsActive(!isOnFire)
     console.log(isOnFire)
+  }
+
+  //define const for the audio file
+  const audio = new Audio(bombTicking)
+
+  function handleStart() {
+    audio.play() // Start playing the audio when the "Start" button is clicked
+  }
+
+  function handleClick() {
+    setIsActive(!isOnFire)
+
+    if (isOnFire) {
+      audio.pause()
+    }
   }
 
   //in CSS make the image tiny
@@ -40,6 +56,7 @@ export default function Level1Component() {
           alt="Activated bomb"
         />
       </div>
+      <button onClick={handleStart}>Start</button>
     </>
   )
 }
--- client/components/Level-2.tsx
+++ client/components/Level-2.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
 export default function Level2Component() {
-  function IsWin2() {}
-  return <p>Level 2</p>
+  return <p>THE END</p>
 }
